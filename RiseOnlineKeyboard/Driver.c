709fbea04d8e867c97b336e38a3dd74b58242a49
Serdar Özkan <serdarozkan41@gmail.com>
2022-04-23 01:15:34 +0300

    map config

--- RiseOnlineKeyboard/Driver.c
+++ RiseOnlineKeyboard/Driver.c
@@ -9,28 +9,24 @@ DriverEntry(
 	PDRIVER_OBJECT     driverObject,
 	PUNICODE_STRING    registryPath)
 {
+
 	UNREFERENCED_PARAMETER(driverObject);
 	UNREFERENCED_PARAMETER(registryPath);
+	KdPrintEx((0, 0, "[Loaded] Driver has been loaded!"));
 
+	return STATUS_SUCCESS;
+
+	// For Map
+	/*
 	NTSTATUS                status = STATUS_SUCCESS;
 	WDF_DRIVER_CONFIG       config;
 
-	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
-
-
 	WDF_DRIVER_CONFIG_INIT(&config, DriverEvtDeviceAdd);
 
-
+	status = WdfDriverCreate(driverObject, registryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
 
 	return status;
-
-
-
-	/* status = WdfDriverCreate(driverObject, registryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
-
-	 return status;*/
-
-
+	*/
 }
 
 _Use_decl_annotations_
