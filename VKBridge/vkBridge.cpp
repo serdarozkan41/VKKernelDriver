30f417aa3dc418225a24442a70c3aae1191f28d5
Serdar Özkan <serdarozkan41@gmail.com>
2022-06-09 03:41:53 +0300

    CHANGE Names

--- VKBridge/vkBridge.cpp
+++ VKBridge/vkBridge.cpp
@@ -1,4 +1,4 @@
-// RiseOnlineBridge.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// VKKernelDriverBridge.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
